e51782cdc013d45bea54f24529045901212e6040
hyper-neutrino <alex.yj.liao@gmail.com>
2022-02-23 00:35:22 -0500

    add account command

---
+++ src/commands/account.js
@@ -0,0 +1,32 @@
+import { config } from "../config.js";
+import { get_money } from "../db.js";
+import { emojis } from "../utils.js";
+
+export const command = {
+    name: "account",
+    description: "View account information for yourself or another user.",
+    type: "CHAT_INPUT",
+    options: [
+        {
+            name: "user",
+            description: "the user to view",
+            type: "USER",
+            required: false,
+        },
+    ],
+};
+
+export async function execute(interaction) {
+    const member = await interaction.options.getMember("user");
+    await interaction.reply({
+        embeds: [
+            {
+                title: `${member.user.tag}'s account`,
+                description: `${member} has ${await get_money(member.id)} ${
+                    emojis.coin
+                }`,
+                color: config.color,
+            },
+        ],
+    });
+}
